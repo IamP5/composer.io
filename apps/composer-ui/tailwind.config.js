225645c50aaf45ab834bdfbcead750258bca39b2
Tuba <brunogabrielsd@hotmail.com>
2023-01-20 15:10:17 -0300

    build(ui): tailwind configuration

---
+++ apps/composer-ui/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'primary': '#3C6255',
+      'secondary': '#61876E',
+      'tertiary': '#A6BB8D',
+      'text': '#EAE7B1',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
