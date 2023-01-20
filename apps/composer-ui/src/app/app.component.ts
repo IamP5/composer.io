08725be2919f7dc2227b62efee44ee8a58e292f2
Tuba <brunogabrielsd@hotmail.com>
2023-01-20 15:00:41 -0300

    feat(ui): bootstraping with standalone component

--- apps/composer-ui/src/app/app.component.ts
+++ apps/composer-ui/src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'composer-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  template: `<router-outlet></router-outlet>`,
 })
-export class AppComponent {
-  title = 'composer-ui';
-}
+export class AppComponent { }
---
+++ apps/composer-ui/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Route } from "@angular/router";
+
+export const APP_ROUTES = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+]satisfies Route[]
